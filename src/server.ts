cdda858c4670bff35e0e240360f5c6b760ef1e04
goyaldeepak13 <deepakgoyalbari@gmail.com>
2024-06-08 21:01:43 +0530

    test

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ const PORT = Number(process.env.PORT) || 3000
 const start = async () => {
     const payload = await getPayloadClient({
          initOptions: {
-            express: app,
+            express: app ,
             onInit: async (cms) => {
                 cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
             },
